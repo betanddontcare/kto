b800e695543a90dd3fc8b72ac4b0b6ed09b7e02b
Igor Betkier <ibetkier.hova@gmail.com>
2017-08-22 15:38:35 +0200

    vehicle suspension

--- js/action.js
+++ js/action.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 //CLICK ACTION
     $("#transporter").click(function(){
         
+        clearInterval(shimmer);
         $("section").removeClass();
 		$("section").addClass("transporter");
         $("button").remove();
@@ -42,17 +43,15 @@ $(document).ready(function(){
 //FIRST H3 ACTION AFTER MOUSE OVER
             $('h3:eq(0)').hover(function(){
                 
-                clearInterval(shimmer);
                 $(this).addClass('bigger');    
-                $('.triangle').css('animation-duration' , '0s');
                 $('.transporter .t-31').removeClass('t-31').addClass('r31');
                 $('.transporter .t-32').removeClass('t-32').addClass('r32');
                 $('.transporter .t-33').removeClass('t-33').addClass('r33');
                 $('.transporter .t-34').removeClass('t-34').addClass('r34');
                 $('.transporter .t-35').removeClass('t-35').addClass('r35');
                 $('.transporter .t-42').removeClass('t-42').addClass('r42');
-                $('.triangle:lt(30)').removeClass('glimmer').addClass('transparent');
-                $('.triangle:gt(34):not(.r42)').removeClass('glimmer').addClass('transparent'); 
+                $('.triangle:lt(30)').addClass('transparent');
+                $('.triangle:gt(34):not(.r42)').addClass('transparent'); 
             })
 //FIRST H3 ACTION AFTER MOUSE OUT
             $('h3:eq(0)').mouseout(function(){
@@ -64,8 +63,8 @@ $(document).ready(function(){
                 $('.transporter .r34').removeClass('r34').addClass('t-34');
                 $('.transporter .r35').removeClass('r35').addClass('t-35');
                 $('.transporter .r42').removeClass('r42').addClass('t-42');
-                $('.triangle:lt(30)').removeClass('transparent').addClass('glimmer');
-                $('.triangle:gt(34):not(.r42)').removeClass('transparent').addClass('glimmer');
+                $('.triangle:lt(30)').removeClass('transparent');
+                $('.triangle:gt(34):not(.r42)').removeClass('transparent');
             })   
 //SECOND H3 CREATING
             setTimeout(function() {
@@ -76,13 +75,11 @@ $(document).ready(function(){
 //SECOND H3 ACTION AFTER MOUSE OVER
                 $('h3:eq(1)').hover(function(){
                 
-                    clearInterval(shimmer);
                     $(this).addClass('bigger');    
-                    $('.triangle').css('animation-duration' , '0s');
                     $('.transporter .t-22').removeClass('t-22').addClass('r22');
                     $('.transporter .t-23').removeClass('t-23').addClass('r23');
-                    $('.triangle:lt(21)').removeClass('glimmer').addClass('transparent');
-                    $('.triangle:gt(22)').removeClass('glimmer').addClass('transparent'); 
+                    $('.triangle:lt(21)').addClass('transparent');
+                    $('.triangle:gt(22)').addClass('transparent'); 
                 })
 //SECOND H3 ACTION AFTER MOUSE OUT
                 $('h3:eq(1)').mouseout(function(){
@@ -90,8 +87,8 @@ $(document).ready(function(){
                     $(this).removeClass('bigger');
                     $('.transporter .r22').removeClass('r22').addClass('t-22');
                     $('.transporter .r23').removeClass('r23').addClass('t-23');
-                    $('.triangle:lt(21)').removeClass('transparent').addClass('glimmer');
-                    $('.triangle:gt(22)').removeClass('transparent').addClass('glimmer'); 
+                    $('.triangle:lt(21)').removeClass('transparent');
+                    $('.triangle:gt(22)').removeClass('transparent'); 
                 }) 
             }, 2000);          
 //THIRD H3 CREATING
@@ -103,21 +100,32 @@ $(document).ready(function(){
 //THIRD H3 ACTION AFTER MOUSE OVER
                 $('h3:eq(2)').hover(function(){
                 
-                    clearInterval(shimmer);
-                    $(this).addClass('bigger');    
-                    var light = [51, 52, 55, 56];
-                    for(x=0; x <= light.length; x++) {
-	                   $('.triangle:eq('+ light[x] + ')').addClass('rolling-light');
+                    $(this).addClass('bigger');   
+                    var light = [0, 4, 44, 45, 48, 49, 51, 52, 55, 56];
+                    for(x=0; x <= light.length; x++) {                  
+                        $('.triangle:eq('+ light[x] + ')').addClass('rolling-light');
                     }
-
-                    var dark = [4, 5, 50, 53, 54, 57];
+                    var dark = [1, 5, 42, 43, 46, 47, 50, 53, 54, 57];
                     for(x=0; x <= dark.length; x++) {
-                    $('.triangle:eq('+ dark[x] + ')').addClass('rolling-dark');
-
-}
-                   
+                        $('.triangle:eq('+ dark[x] + ')').addClass('rolling-dark');
+                    }
+                    $('.triangle:lt(42):not(.t-1 , .t-2 , .t-5 , .t-6)').addClass('transparent').addClass('shudder');
                     
                 })
+//THIRD H3 ACTION AFTER MOUSE OUT
+                $('h3:eq(2)').mouseout(function(){
+                
+                    $(this).removeClass('bigger');
+                    var light = [0, 4, 44, 45, 48, 49, 51, 52, 55, 56];
+                    for(x=0; x <= light.length; x++) {                  
+                        $('.triangle:eq('+ light[x] + ')').removeClass('rolling-light');
+                    }
+                    var dark = [1, 5, 42, 43, 46, 47, 50, 53, 54, 57];
+                    for(x=0; x <= dark.length; x++) {
+                        $('.triangle:eq('+ dark[x] + ')').removeClass('rolling-dark');
+                    }
+                    $('.triangle:lt(42):not(.t-1 , .t-2 , .t-5 , .t-6)').removeClass('transparent').removeClass('shudder');
+                }) 
             }, 4000);
         }, 5000);
 	})
@@ -136,3 +144,4 @@ $(document).ready(function(){
 	    $(".main-stage").toggleClass("glimmer");
 	}, 3000);	
 });
+
